516610abf2587255b0ac9319e4d2fcb0db0eb375
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-10-17 19:05:44 +0900

    デバイス用にすトロングパラメーター作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  # before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?  # 追加
+
+  private
+
+  #要確認
+# デプロイする際に設定
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+  #   end
+  # end
+
+  # 追加済みの configure_permitted_parameters メソッド
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, :email]
+    )
+  end
 end
