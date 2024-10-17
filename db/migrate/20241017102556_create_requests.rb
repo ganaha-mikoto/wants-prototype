d56c5859d0c5591327fa6d74a88d7f0ab8eb9cbc
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-10-17 19:27:03 +0900

    リクエストテーブルをマイグレーション

--- db/migrate/20241017102556_create_requests.rb
+++ db/migrate/20241017102556_create_requests.rb
@@ -1,6 +1,14 @@
 class CreateRequests < ActiveRecord::Migration[7.0]
   def change
     create_table :requests do |t|
+      t.string :title, null: false
+      t.text :description, null: false
+      t.integer :category_id, null: false
+      t.integer :max_price, null: false
+      t.integer :min_price, null: false
+      t.references :user, null: false, foreign_key: true
+      t.string :image
+      t.integer :shipping_charge_id, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_17_093608) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_17_102556) do
+  create_table "requests", charset: "utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.integer "category_id", null: false
+    t.integer "max_price", null: false
+    t.integer "min_price", null: false
+    t.bigint "user_id", null: false
+    t.string "image"
+    t.integer "shipping_charge_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_requests_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -29,4 +43,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_17_093608) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "requests", "users"
 end
