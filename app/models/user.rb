82d34323f15cd54f546648bfd745a1c1510ddf4d
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-10-17 18:44:18 +0900

    userテーブルの作成とuserモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+class Request < ApplicationRecord
+  belongs_to :user
+  has_many :products
+  has_many :comments
+  has_one_attached :image
+  has_many :histories
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :shipping_charge
+
+  validates :title, :description, :category_id, :shipping_charge_id, presence: true
+  validates :category_id, :shipping_charge_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :max_price, :min_price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "must be between 300 and 9999999" }
 end
