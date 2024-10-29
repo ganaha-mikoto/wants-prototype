7f888f10715055694b632d9e4477bbd6ea8aa547
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-10-29 20:46:03 +0900

    リクエスト失敗時の画面

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -14,7 +14,7 @@ class RequestsController < ApplicationController
     if @request.save
       redirect_to root_path, notice: 'リクエストが作成されました。'
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
