2155d5667726987fbb558d684e5c588bf0554433
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-11-07 13:46:14 +0900

    productsコントローラーへのルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/new'
-  get 'products/create'
-  get 'products/show'
-  get 'products/edit'
-  get 'products/update'
-  get 'products/destroy'
   devise_for :users
-  resources :requests 
+  resources :requests do
+    resources :prodects, only: [:index, :create]
+  end
   root to: "requests#index"
 end
