3aad0363794ad2c1a945ad47d9ad429cbc7f374a
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-11-07 13:41:46 +0900

    productsコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/new'
+  get 'products/create'
+  get 'products/show'
+  get 'products/edit'
+  get 'products/update'
+  get 'products/destroy'
   devise_for :users
   resources :requests 
   root to: "requests#index"
