fa31e91c25d06f861c1c94eedbf1561cbff32ad0
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-11-07 14:08:27 +0900

    [upgrade]productsコントローラーのインスタンス変数を定義

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,21 @@
 class ProductsController < ApplicationController
   def index
+    @prodeucts = Product.all
   end
 
   def new
+    @prodeucts = Product.new
   end
 
   def create
+    @product = Product.new(product_params) # リクエストの作成
+    @product.user = current_user # ログインユーザーをリクエストのユーザーに設定
+
+    if @product.save
+      redirect_to root_path, notice: '出品が完了しました。'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
